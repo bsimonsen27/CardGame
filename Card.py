808d51fa31ec189b19c212208a4bd1a0f7001798
Brendon Simonsen <brendonsimonsen@mail.weber.edu>
2021-04-14 08:52:37 -0600

    created card class

---
+++ Card.py
@@ -0,0 +1,11 @@
+# class is a blueprint of an Object
+class Card:
+    #attributes
+    def __init__(self, name, cost):
+        self.name = name
+        self.cost = cost
+
+    #behaviors 
+    def printCard(self):
+        print ("Name:", self.name)
+        print ("Cost:", self.cost)
