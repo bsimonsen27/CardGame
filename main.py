99501e4756e8077d8f33b73e828c96738c6d167d
Brendon Simonsen <brendonsimonsen@mail.weber.edu>
2021-04-19 09:13:32 -0600

    created loop for adding cards to deck

--- main.py
+++ main.py
@@ -5,14 +5,27 @@ def main():
 
     deck=[]
 
-    acornBearer = Minion('Acorn Bearer', 1, 2, 1)
-    deck.append(acornBearer)
-    crystalPower = Card('Crystal Power', 1)
-    deck.append(crystalPower)
-    razorBoar = Minion('Razor Boar', 2, 2, 2)
-    deck.append(razorBoar)
-    vileCall = Card('Vile Call', 3)
-    deck.append(vileCall)
+    cards = open("Cards.txt", 'r')
+    
+    for card in cards:
+        cardInfo = card.split(',')
+        nameOfCard = cardInfo[0]
+        costOfCard = cardInfo[1]
+
+        lengthOfList = len(cardInfo)
+
+        if lengthOfList == 2:
+            cardData = Card(nameOfCard, costOfCard)
+
+        if lengthOfList == 4:
+            attackP = cardInfo[2]
+            healthP = cardInfo[3]
+            cardData = Minion(nameOfCard, costOfCard, attackP, healthP)
+        
+        deck.append(cardData)
+
+
+    cards.close()
 
     
     for card in deck:
